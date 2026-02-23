6fc2f885bbf35eb4d83dafa4e39d61ca25aad682
GeneralCKW <claywherley@gmail.com>
2026-02-22 22:00:45 -0600

    minor changes to  database and main files in backend for testing purposes

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.config import DATABASE_URL
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Create engine
 engine = create_engine(DATABASE_URL)
+
+# Session Factory
 SessionLocal = sessionmaker(bind=engine)
 
+# Base class for models
 Base = declarative_base()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from app.routes import auth, assessments, simulation # Example routes
+from app.database import engine, Base
+from app.models import user # Import all your models
+
+# Create tables
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="SecureScope AI")
 
