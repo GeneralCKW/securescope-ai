77ddb30f427ef8c225db74125f19557c9d027d8e
GeneralCKW <claywherley@gmail.com>
2026-02-22 21:34:04 -0600

    new file:   backend/.env 	modified:   backend/app/models/user.py

---
+++ backend/app/models/user.py
@@ -0,0 +1,13 @@
+import uuid
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email = Column(String, unique=True, nullable=False)
+    pasword_hash = Column(string, nullable=False)
+    role = Column(String, default="analyst")
+    
