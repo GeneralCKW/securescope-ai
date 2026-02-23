b802d97d2ab416090ea0012bdea4f7bc54ad23ef
GeneralCKW <claywherley@gmail.com>
2026-02-23 16:40:23 -0600

    updated main for routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,16 @@ from fastapi import FastAPI
 from app.routes import auth, assessments, simulation # Example routes
 from app.database import engine, Base
 from app.models import user, organization, assessment, question, response, vulnerability, attack_path, attack_step # Import all your models
+from app.routers import users, organizations # Import Routers
 
 # Create tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="SecureScope AI")
+
+app.include_router(users.router)
+app.include_router(organizations.router)
+
 
 @app.get("/")
 def read_root():
