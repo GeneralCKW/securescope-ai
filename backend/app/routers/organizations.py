8d451eec65f31ab124e2827184de1a6564e31f01
GeneralCKW <claywherley@gmail.com>
2026-03-27 01:45:31 -0500

    updated organizations.py to require authorization

--- backend/app/routers/organizations.py
+++ backend/app/routers/organizations.py
@@ -3,13 +3,17 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models.organization import Organization
 from app.schemas.organization import OrganizationCreate, OrganizationResponse
-
+from app.models.user import User
+from app.auth import get_current_user
 import uuid
 
 router = APIRouter(prefix="/organizations", tags=["Organizations"])
 
 @router.post("/", response_model=OrganizationResponse)
-def create_org(org: OrganizationCreate, db: Session = Depends(get_db)):
+def create_org(
+    org: OrganizationCreate, 
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)):
     db_org = Organization(
         id=uuid.uuid4(),
         name=org.name,
@@ -22,5 +26,8 @@ def create_org(org: OrganizationCreate, db: Session = Depends(get_db)):
     return db_org
 
 @router.get("/", response_model=list[OrganizationResponse])
-def get_orgs(db: Session = Depends(get_db)):
+def get_orgs(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     return db.query(Organization).all()
